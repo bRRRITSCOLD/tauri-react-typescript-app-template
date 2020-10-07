fe56c6c4d92299e546c0e0b77ea64a0a1157aba8
Blaine Richardson <blainerrichardson@gmail.com>
2020-10-06 21:09:41 -0500

    feat(tauri): adding tauri for making cross platform desktop apps

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,65 @@
+// node_modules
 import React from 'react';
-import logo from './logo.svg';
+import {
+  AppBar, Button, IconButton, Toolbar,
+} from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+import {
+  Redirect, Switch, Route, Link,
+} from 'react-router-dom';
+import * as tauriDialog from 'tauri/api/dialog'
+
+// css
 import './App.css';
+
+function Nav() {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton edge="start" color="inherit" aria-label="menu">
+          <MenuIcon />
+        </IconButton>
+        <Button component={Link} to="/" color="inherit">
+          Index
+        </Button>
+        <Button component={Link} to="/about" color="inherit">
+          About
+        </Button>
+      </Toolbar>
+    </AppBar>
+  )
+}
+
+function Index() {
+  async function onClick() {
+    const dirs = await tauriDialog.open({ directory: true });
+    console.log(dirs);
+  }
+
+  return (
+    <div className="App">
+      <Button type="button" onClick={onClick}></Button>
+    </div>
+  )
+}
+
+function About() {
+  return (
+    <div className="App">
+      About
+    </div>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Nav />
+      <Switch>
+        <Route exact path="/" component={Index} />
+        <Route exact path="/about" component={About} />
+      </Switch>
+    </>
   );
 }
 
