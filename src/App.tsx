4f6cc98cb435c7e7eb8050c80e1f78af00673bfa
Blaine Richardson <blainerrichardson@gmail.com>
2020-10-07 20:37:33 -0500

    feat(table): adding very basic table as example - will expand on later

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ import * as tauriFs from 'tauri/api/fs'
 
 // components
 import { GridContainer, GridItem } from './components/UI/Grid';
+import { Table } from './components/UI/Table/Table';
 
 // css
 import './App.css';
@@ -34,11 +35,63 @@ function Nav() {
   )
 }
 
+
+const columns = [
+  { id: 'name', label: 'Name', minWidth: 170 },
+  { id: 'code', label: 'ISO\u00a0Code', minWidth: 100 },
+  {
+    id: 'population',
+    label: 'Population',
+    minWidth: 170,
+    align: 'right',
+    format: (value: any) => value.toLocaleString('en-US'),
+  },
+  {
+    id: 'size',
+    label: 'Size\u00a0(km\u00b2)',
+    minWidth: 170,
+    align: 'right',
+    format: (value: any) => value.toLocaleString('en-US'),
+  },
+  {
+    id: 'density',
+    label: 'Density',
+    minWidth: 170,
+    align: 'right',
+    format: (value: any) => value.toFixed(2),
+  },
+];
+
+function createData(name: any, code: any, population: any, size: any) {
+  const density = population / size;
+  return { name, code, population, size, density };
+}
+
+const rows = [
+  createData('India', 'IN', 1324171354, 3287263),
+  createData('China', 'CN', 1403500365, 9596961),
+  createData('Italy', 'IT', 60483973, 301340),
+  createData('United States', 'US', 327167434, 9833520),
+  createData('Canada', 'CA', 37602103, 9984670),
+  createData('Australia', 'AU', 25475400, 7692024),
+  createData('Germany', 'DE', 83019200, 357578),
+  createData('Ireland', 'IE', 4857000, 70273),
+  createData('Mexico', 'MX', 126577691, 1972550),
+  createData('Japan', 'JP', 126317000, 377973),
+  createData('France', 'FR', 67022000, 640679),
+  createData('United Kingdom', 'GB', 67545757, 242495),
+  createData('Russia', 'RU', 146793744, 17098246),
+  createData('Nigeria', 'NG', 200962417, 923768),
+  createData('Brazil', 'BR', 210147125, 8515767),
+];
+
 function Index() {
   const [files, setFiles] = useState([]);
+  const [dirName, setDirName] = useState('');
 
   async function onClick() {
     const dir: any = await tauriDialog.open({ directory: true });
+    setDirName(dir);
     const readDir: any[] = await tauriFs.readDir(dir, { recursive: true } as any);
     console.log('read');
     setFiles([
@@ -47,19 +100,13 @@ function Index() {
   }
   return (
     <div className="App">
-      <GridContainer direction="column">
+      <GridContainer alignItems="center" direction="column">
         <div>
           <Button type="button" onClick={onClick}>Select Directory</Button>
         </div>
-        {
-          files.length
-            ? files.map((file: any) => 
-              <GridItem>
-                {JSON.stringify(file, null, 2)}
-              </GridItem>
-            )
-            : ''
-        }
+        <GridItem xs={8}>
+          <Table columns={columns} rows={rows}/>
+        </GridItem>
       </GridContainer>
     </div>
   )
---
+++ src/components/UI/Table/Table.tsx
@@ -0,0 +1,110 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import { Table as MUITable} from '@material-ui/core';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TablePagination from '@material-ui/core/TablePagination';
+import TableRow from '@material-ui/core/TableRow';
+
+const useStyles = makeStyles({
+  root: {
+    width: '100%',
+  },
+  container: {
+    maxHeight: 440,
+  },
+});
+
+export interface TablePropsColumnInterface {
+  id: string,
+  label: string,
+  minWidth?: number,
+  align?: string;
+  format?: (value: any) => any;
+}
+
+export interface TablePropsInterface {
+  columns: TablePropsColumnInterface[];
+  rows: any[];
+}
+
+export function Table(props: TablePropsInterface) {
+  // deconstruct for ease
+  const { columns, rows } = props;
+  const classes = useStyles();
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  const handleChangePage = (_event: any, newPage: any) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event: any) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
+  return (
+    <Paper className={classes.root}>
+      <TableContainer className={classes.container}>
+        <MUITable stickyHeader aria-label="sticky table">
+          <TableHead>
+            <TableRow>
+              {columns.map((column: TablePropsColumnInterface) => (
+                <TableCell
+                  key={column.id}
+                  align={column.align ? column.align as any : 'right'}
+                  style={{ minWidth: column.minWidth ? column.minWidth : 100 }}
+                >
+                  {column.label}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {/* {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row: any) => {
+              return (
+                <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                  {columns.map((column: any) => {
+                    const value: any = row[column.id];
+                    return (
+                      <TableCell key={column.id} align={column.align}>
+                        {column.format && typeof value === 'number' ? column.format(value) : value}
+                      </TableCell>
+                    );
+                  })}
+                </TableRow>
+              );
+            })} */}
+            {rows.map((row: any) => {
+              return (
+                <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                  {columns.map((column: any) => {
+                    const value: any = row[column.id];
+                    return (
+                      <TableCell key={column.id} align={column.align}>
+                        {column.format && typeof value === 'number' ? column.format(value) : value}
+                      </TableCell>
+                    );
+                  })}
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </MUITable>
+      </TableContainer>
+      {/* <TablePagination
+        rowsPerPageOptions={[10, 25, 100]}
+        component="div"
+        count={rows.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onChangePage={handleChangePage}
+        onChangeRowsPerPage={handleChangeRowsPerPage}
+      /> */}
+    </Paper>
+  );
+}
